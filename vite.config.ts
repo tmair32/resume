7dca5b785237a6436ea87d59630194c06bd9378b
tmair32 <tmair32@gmail.com>
2022-10-07 13:57:09 +0900

    :package: Setting Unplugin Auto Import & Unplugin Components

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 import eslintPlugin from "vite-plugin-eslint";
 import Pages from "vite-plugin-pages";
 import vue from "@vitejs/plugin-vue";
@@ -8,7 +10,35 @@ import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
+    AutoImport({
+      imports: [
+        // presets
+        "vue",
+        "vue-router",
+        // custom
+        {
+          "@vueuse/core": [
+            // named imports
+            "useMouse", // import { useMouse } from '@vueuse/core',
+            // alias
+            "useFetch",
+          ],
+          axios: [
+            // default imports
+            ["default", "axios"], // import { default as axios } from 'axios',
+          ],
+        },
+      ],
+      dts: "src/auto-imports.d.ts",
+    }),
+    Components({
+      extensions: ["vue"],
+      include: [/\.vue$/, /\.vue\?vue/],
+      deep: true,
+      dts: true,
+    }),
     eslintPlugin(),
     Pages({
       dirs: [
@@ -22,6 +52,9 @@ export default defineConfig({
     vue(),
     WindiCSS(),
   ],
+  optimizeDeps: {
+    include: ["vue", "vue-router", "@vueuse/core"],
+  },
   resolve: {
     alias: {
       "~/": `${resolve(__dirname, "src")}/`,
