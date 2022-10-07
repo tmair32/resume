52ecdcb3383fe68c6e6d064d5f65b70cbc12e61b
tmair32 <tmair32@gmail.com>
2022-10-07 13:53:18 +0900

    :package: Setting Vite Plugin Pages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import autoRoutes from "virtual:generated-pages";
+import {
+  createRouter,
+  createWebHistory,
+  RouterScrollBehavior,
+} from "vue-router";
+
 import "virtual:windi.css";
 import "virtual:windi-devtools";
 import "~/assets/styles/_index.scss";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+// Router
+const routes = autoRoutes.map((route) => {
+  return {
+    ...route,
+    alias: route.path.endsWith("/") ? `${route.path}index` : route.path,
+  };
+});
+
+const scrollBehavior: RouterScrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) {
+    return savedPosition;
+  }
+  if (to.hash) {
+    return { el: to.hash };
+  }
+  return { top: 0, behavior: "smooth" };
+};
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior,
+});
+
+app.use(router);
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+
 import eslintPlugin from "vite-plugin-eslint";
+import Pages from "vite-plugin-pages";
+import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [eslintPlugin(), vue(), WindiCSS()],
+  plugins: [
+    eslintPlugin(),
+    Pages({
+      dirs: [
+        {
+          dir: "src/pages",
+          baseRoute: "",
+        },
+      ],
+      extensions: ["vue"],
+    }),
+    vue(),
+    WindiCSS(),
+  ],
+  resolve: {
+    alias: {
+      "~/": `${resolve(__dirname, "src")}/`,
+    },
+  },
 });
